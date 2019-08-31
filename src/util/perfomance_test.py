e24f1ac071cab24048b6df553f42bbd4863f5bc0
chul <chuuuul@naver.com>
2019-08-31 14:30:59 +0900

    성능측정 구현완료

--- src/util/perfomance_test.py
+++ src/util/perfomance_test.py
@@ -17,6 +17,7 @@ class Performance:
     display_size_x = None
     display_size_y = None
     total_size = None
+    last_path = []
 
     show_img = cv2.imread('../ballon.jpg', cv2.IMREAD_COLOR)
 
@@ -31,7 +32,11 @@ class Performance:
 
 
     def calc_error_rate(self,idx):
-        error_distance = abs(self.real_coordinates[idx] - self.gaze_coordinates[idx])
+        error_distance_x = self.real_coordinates[idx][0] - self.gaze_coordinates[idx][0]
+        error_distance_y = self.real_coordinates[idx][1] - self.gaze_coordinates[idx][1]
+
+        error_distance = np.sqrt(error_distance_x * error_distance_x + error_distance_y * error_distance_y)
+
 
         error_rate = error_distance / self.total_size * 100
         self.error_rates.append(error_rate)
@@ -39,7 +44,7 @@ class Performance:
 
     def calc_total_error_rate(self,idx):
         sum_rate = 0
-        for i in range(0,idx+1):
+        for i in range(0,idx):
             sum_rate = sum_rate + self.error_rates[i]
 
         if sum_rate == 0:
@@ -58,45 +63,70 @@ class Performance:
             self.gaze_coordinates.append( param )
             self.calc_error_rate(self.save_index)
             self.save_index = self.save_index + 1
+            self.write_history(x,y)
 
 
             # var1 and var2 비교 후 계산
 
-            cv2.circle(self.show_img, (x, y), 3, (255, 0, 0), -1)
 
-        elif event == cv2.EVENT_RBUTTONDBLCLK:
+        elif event == cv2.EVENT_MBUTTONDOWN:
             self.calc_total_error_rate(self.save_index)
 
+    def write_history(self,x,y):
+        self.last_path = (x, y)
+
+    def draw_mark(self):
+        if self.save_index != 0:
+            cv2.circle(self.show_img, self.last_path, 7, (255, 0, 0), -1)
 
 
 
 
+########### 추가 ##################
+import time  # time 라이브러리
 
+###################################
+perf = Performance(1280, 720)
 
-cali = Calibration()
-perf = Performance(cali.Const_Display_X, cali.Const_Display_Y)
-
-# img = cv2.imread('../ballon.jpg', cv2.IMREAD_COLOR)
-
-cv2.namedWindow('image2', cv2.WINDOW_NORMAL)  # auto resized
-cv2.setWindowProperty('image2', cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FREERATIO )
-cv2.setMouseCallback('image2', perf.mouse_callback,param=[1,2])
-
-capture = cv2.VideoCapture(0)
-capture.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
-capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
-
-while True:
-    ret, frame = capture.read()
-    perf.show_img = frame
-
-    cv2.imshow('image2', perf.show_img)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-cv2.destroyAllWindows()
+cam = cv2.VideoCapture(-1)  # 카메라 생성
+# cam.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+# cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+# 윈도우 생성 및 사이즈 변경
+cv2.namedWindow('CAM_Window')
+cv2.setMouseCallback('CAM_Window', perf.mouse_callback,param=[10,10])
 
 
 
+prevTime = 0  # 이전 시간을 저장할 변수
+while (True):
 
+    ret, perf.show_img = cam.read()
+
+    curTime = time.time()
+
+    sec = curTime - prevTime
+    prevTime = curTime
+    fps = 1 / (sec)
+
+
+    # 프레임 수를 문자열에 저장
+    str = "FPS : %0.1f" % fps
+
+    # 표시
+    cv2.putText(perf.show_img, str, (0, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0))
+
+
+    if perf.save_index != 0 :
+        perf.draw_mark()
+    # 얻어온 이미지 윈도우에 표시
+    cv2.imshow('CAM_Window', perf.show_img)
+
+    # 10ms 동안 키입력 대기
+    if cv2.waitKey(10) >= 0:
+        break;
+
+# 윈도우 종려
+cam.release()
+cv2.destroyWindow('CAM_Window')
 
 
