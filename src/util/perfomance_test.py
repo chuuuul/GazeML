954eddd308a776f54550c77c0222cb1afdd862eb
chul <chuuuul@naver.com>
2019-08-31 17:20:09 +0900

    버그 수정

--- src/util/perfomance_test.py
+++ src/util/perfomance_test.py
@@ -13,22 +13,20 @@ class Performance:
     gaze_coordinates = []           # 보고있는 좌표
     error_rates = []                # 에러율
     save_index = 0
-    show_img = None
+
     display_size_x = None
     display_size_y = None
     total_size = None
-    last_path = []
+    last_real_coordinate = []
+    last_gaze_coordinate = []
 
-    show_img = cv2.imread('../ballon.jpg', cv2.IMREAD_COLOR)
+    is_set_callback = False
+
 
     def __init__(self, display_size_x, display_size_y ):
         self.display_size_x = display_size_x
         self.display_size_y = display_size_y
         self.total_size = np.sqrt(self.display_size_x * self.display_size_x + self.display_size_y * self.display_size_y)
-        print(' test ! total size : ', self.total_size)
-
-        pass
-
 
 
     def calc_error_rate(self,idx):
@@ -37,10 +35,10 @@ class Performance:
 
         error_distance = np.sqrt(error_distance_x * error_distance_x + error_distance_y * error_distance_y)
 
-
         error_rate = error_distance / self.total_size * 100
         self.error_rates.append(error_rate)
         print("인덱스 ", idx," 의 에러율 : ", error_rate)
+
 
     def calc_total_error_rate(self,idx):
         sum_rate = 0
@@ -55,78 +53,44 @@ class Performance:
         print("전체 에러율 : ", self.total_rate)
 
 
+    # param 은 캘리브레이션 객체
     def mouse_callback(self,event, x, y, flags, param):
+
         if event == cv2.EVENT_LBUTTONDOWN:
             # event
+            print("Left Button Click !!!")
+            tmp_gaze_coordinate = (param.left_gaze_coordinate + param.right_gaze_coordinate) / 2
+            gaze_coordinate = (int (tmp_gaze_coordinate[0]) , int (tmp_gaze_coordinate[1]))
 
             self.real_coordinates.append( (x,y) )
-            self.gaze_coordinates.append( param )
+            self.gaze_coordinates.append( gaze_coordinate )
             self.calc_error_rate(self.save_index)
             self.save_index = self.save_index + 1
-            self.write_history(x,y)
-
-
-            # var1 and var2 비교 후 계산
+            self.write_last_real_coordinate((x,y))
+            self.write_last_gaze_coordinate(gaze_coordinate)
 
 
         elif event == cv2.EVENT_MBUTTONDOWN:
+            if self.save_index == 0:
+                return
+
             self.calc_total_error_rate(self.save_index)
 
-    def write_history(self,x,y):
-        self.last_path = (x, y)
 
-    def draw_mark(self):
+    def write_last_real_coordinate(self, coordinate):
+        self.last_real_coordinate = coordinate
+
+
+    def write_last_gaze_coordinate(self, coordinate):
+        self.last_gaze_coordinate = coordinate
+
+
+    def draw_real_coordinate_mark(self,img):
         if self.save_index != 0:
-            cv2.circle(self.show_img, self.last_path, 7, (255, 0, 0), -1)
+            cv2.circle(img, self.last_real_coordinate, 7, (255, 0, 0), -1)
 
 
-
-
-########### 추가 ##################
-import time  # time 라이브러리
-
-###################################
-perf = Performance(1280, 720)
-
-cam = cv2.VideoCapture(-1)  # 카메라 생성
-# cam.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
-# cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
-# 윈도우 생성 및 사이즈 변경
-cv2.namedWindow('CAM_Window')
-cv2.setMouseCallback('CAM_Window', perf.mouse_callback,param=[10,10])
-
-
-
-prevTime = 0  # 이전 시간을 저장할 변수
-while (True):
-
-    ret, perf.show_img = cam.read()
-
-    curTime = time.time()
-
-    sec = curTime - prevTime
-    prevTime = curTime
-    fps = 1 / (sec)
-
-
-    # 프레임 수를 문자열에 저장
-    str = "FPS : %0.1f" % fps
-
-    # 표시
-    cv2.putText(perf.show_img, str, (0, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0))
-
-
-    if perf.save_index != 0 :
-        perf.draw_mark()
-    # 얻어온 이미지 윈도우에 표시
-    cv2.imshow('CAM_Window', perf.show_img)
-
-    # 10ms 동안 키입력 대기
-    if cv2.waitKey(10) >= 0:
-        break;
-
-# 윈도우 종려
-cam.release()
-cv2.destroyWindow('CAM_Window')
-
+    def draw_gaze_coordinate_mark(self,img):
+        if self.save_index != 0:
+            cv2.circle(img, self.last_gaze_coordinate, 7, (0, 0, 255), -1)
 
