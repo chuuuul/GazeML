ded8b5d5d0125f6fa064bc136a64069ee455d02d
chul <chuuuul@naver.com>
2019-09-05 21:59:45 +0900

    소켓 전송 완료

--- src/util/gaze_data_receiver.py
+++ src/util/gaze_data_receiver.py
@@ -9,36 +9,42 @@ class GazeDataReceiver:
 
 
     SERVER_IP = '127.0.0.1'
-    SERVER_PORT = 2349
+    SERVER_PORT = 23487
     SIZE = 1024
-    SERVER_ADDR = (SERVER_IP, SERVER_PORT)
+
+
+
 
     def __init__(self):
-
         pass
 
-    def
+    def receive_gaze(self):
 
-    # 접속 정보 설정
+        # 클라이언트 소켓 설정
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
 
+            SERVER_ADDR = (self.SERVER_IP, self.SERVER_PORT)
+            client_socket.connect(SERVER_ADDR)  # 서버에 접속
+            # client_socket.send('hi'.encode())  # 서버에 메시지 전송
 
-    # 클라이언트 소켓 설정
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
+            # receiver = threading.Thread(target=receieve, args=(client_socket,))
+            # receiver.start()
 
+            # def receieve(sock):
+            while True:
+                recvData = client_socket.recv(1024)
+                recvData = recvData.decode('utf-8')
+                print('상대방 :', recvData)
+                #꼬리에붙은 - 구분자 제거
+                # data, trash = str(recvData).split('-', 1)
+                first, second = str(recvData).split(',')
+                print("first : ", first)
+                print("second : ", second)
+                client_socket.send( "ok".encode())
 
+            while True:
+                time.sleep(1)
+                pass
 
-        client_socket.connect(SERVER_ADDR)  # 서버에 접속
-        # client_socket.send('hi'.encode())  # 서버에 메시지 전송
-
-        # receiver = threading.Thread(target=receieve, args=(client_socket,))
-        # receiver.start()
-
-        # def receieve(sock):
-        while True:
-            recvData = sock.recv(1024)
-            print('상대방 :', recvData.decode('utf-8'))
-            first, second = recvData.split(',')
-
-        while True:
-            time.sleep(1)
-            pass
+gazeDataReceiver =  GazeDataReceiver()
+gazeDataReceiver.receive_gaze()
