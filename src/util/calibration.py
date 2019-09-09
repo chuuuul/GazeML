545ef057bf1258e29e0ad38f1347a3e10613af57
chul <chuuuul@naver.com>
2019-09-09 18:23:24 +0900

    캘리브레이션 최적화

--- src/util/calibration.py
+++ src/util/calibration.py
@@ -2,14 +2,12 @@ import queue
 import cv2
 import numpy as np
 import threading
+import time
 
 
 class Calibration:
 
-    tmp_var = 2
-
     def __init__(self):
-
         pass
 
     is_face_detect = False
@@ -19,8 +17,6 @@ class Calibration:
     is_fail = False
 
     is_full_screen = False
-
-
 
     ###################################### Start Cali #############################################
     Const_Cali_Window_name = 'canvas'
@@ -34,11 +30,13 @@ class Calibration:
     Const_Cali_Num_X = 4  # 캘리브레이션 포인트 x 갯수
     Const_Cali_Num_Y = 4  # 캘리브레이션 포인트 y 갯수
     Const_Cali_Radius = 30  # 캘리브레이션 포인트 원 크기
-    Const_Cali_Resize_Radius = 7  # 캘리브레이션 포인트가 가장 작을 때 원 크기
+    Const_Cali_Goal_Radius = 4  # 캘리브레이션 포인트가 가장 작을 때 원 크기
 
-    Const_Cali_Unit_Time = 60  # 캘리브레이션 한 번 표현 소요 시간 (쓰레드 반복 시간, 프레임) # 1/60 초마다 실행 # 50 일 때 문제발생
-    Const_Cali_Move_Duration = 0.2  # 캘리브레이션 원 이동 횟수       # 이동 할 때 (Unit_Time * Move_Duration)만큼 소요
-    Const_Cali_Capture_Duration = 0.2  # 캘리브레이션 원 줄어드는 횟수    # 줄어들 때 (Unit_Time * Move_Duration)만큼 소요
+    Const_Cali_Reduce_Num = 20      # 줄어들 횟수
+    Const_Cali_Reduce_Sleep = 0.03  # 한번 줄어드는데 걸리는 시간
+
+    Const_Cali_Move_Num = 20        # 이동할 횟수
+    Const_Cali_Move_Sleep = 0.03    # 한번 이동하는데 걸리는 시간
 
     Const_Cali_Margin_X = 50  # 모니터 모서리에서 떨어질 X 거리
     Const_Cali_Margin_Y = 50  # 모니터 모서리에서 떨어질 Y 거리
@@ -81,6 +79,7 @@ class Calibration:
     correct_point = []
 
     sequence = queue.Queue()
+    current_image = None
 
 
     def start_cali(self,):
@@ -92,104 +91,110 @@ class Calibration:
         for i in range(0, self.Const_Cali_Num_X * self.Const_Cali_Num_Y):
             self.sequence.put_nowait(i)
 
-        img = self.init_canvas()
+        background = self.init_canvas()
+        self.current_image = background.copy()
         self.init_cali()
 
         # 큐의 순서대로 캘리브레이션 시작
         index = self.sequence.get_nowait()
 
-        self.resize_figure(img, self.Cali_Center_Points[index], self.Const_Cali_Radius, self.Const_Cali_Capture_Duration)
+        while(True):
+            self.resize_figure(background, self.Cali_Center_Points[index], self.Const_Cali_Radius)
 
-        # 문제점 : waitkey로 키 입력 받으려고 변수 저장 후 비교해서 다르면 waitkey를 다시 하게 되는데 그 순간 먹통(쓰레드랑관련?)
-        cv2.waitKey(0)
-        print("pressed key!! Exit calibration!")
-        self.is_finish = True
-        self.is_fail = True
-        self.close_window(self.Const_Cali_Window_name)
-        return
+            previous_index = index
+            index = self.sequence.get_nowait()
+            self.move_figure(background, self.Cali_Center_Points[previous_index], self.Cali_Center_Points[index])
 
-    def move_figure(self, img, start_point, end_point, current_point, duration, count=0):
+            if self.sequence.empty() == True:
+                # 마지막_포인트_줄어드는_애니메이션
+                self.resize_figure(background, self.Cali_Center_Points[index], self.Const_Cali_Radius)
+                break
 
-        copy_img = img.copy()
+        self.is_finish = True  # 종료플레그
+        print("Complete Calibration!!")
+
+        # # 문제점 : waitkey로 키 입력 받으려고 변수 저장 후 비교해서 다르면 waitkey를 다시 하게 되는데 그 순간 먹통(쓰레드랑관련?)
+        # print("pressed key!! Exit calibration!")
+        # self.is_finish = True
+        # self.is_fail = True
+        # return
+
+
+    def resize_figure(self, img, point, radius):
+
+        current_radius = radius
+        count = 0
+
+
+        to_resize_radius = self.Const_Cali_Radius - self.Const_Cali_Goal_Radius       # 총_줄어들어야하는_크기
+        # 줄어들크기 / 줄어들_횟수 = 한번에줄을크기
+        resize_once_radius = to_resize_radius / self.Const_Cali_Reduce_Num
+
+
+        while(count != self.Const_Cali_Reduce_Num + 1 ):
+
+            #도화지_초기화
+            copy_img = img.copy()
+
+            while (self.is_face_detect == False):
+                continue
+
+            # 원_그리기
+            self.draw_circle(copy_img, point, current_radius)
+            self.draw_cross(copy_img, point)
+
+            #줄어든 횟수 체크 / 다음에_그릴_반지름_계산
+            count = count + 1
+            current_radius = current_radius - resize_once_radius
+
+            #그림표시
+            self.display_canvas(copy_img)
+
+            time.sleep(self.Const_Cali_Reduce_Sleep)
+
+
+        ############ 캘리브레이션 순간! ############
+        self.left_eyeball_captured_data.append(self.left_eyeball_centre)
+        self.right_eyeball_captured_data.append(self.right_eyeball_centre)
+
+        self.left_iris_captured_data.append(self.left_iris_centre)
+        self.right_iris_captured_data.append(self.right_iris_centre)
+
+        print ("left_iris_captured_data : ",self.left_eyeball_captured_data)
+        ##########################################
+
+
+
+
+
+
+    def move_figure(self, img, start_point, end_point ):
+
+
+        count = 0
 
         to_move_x = (end_point[0] - start_point[0])
         to_move_y = (end_point[1] - start_point[1])
 
-        move_once_x = to_move_x / (duration * self.Const_Cali_Unit_Time)
-        move_once_y = to_move_y / (duration * self.Const_Cali_Unit_Time)
+        current_point = (start_point[0],start_point[1])
 
-        updated_current_point = (current_point[0] + move_once_x, current_point[1] + move_once_y)
+        # 한번 그릴때마다 이동 할 거리
+        move_once_x = to_move_x / (self.Const_Cali_Move_Num)
+        move_once_y = to_move_y / (self.Const_Cali_Move_Num)
 
-        self.draw_circle(copy_img, current_point, self.Const_Cali_Radius)
-        self.draw_cross(copy_img, current_point)
+        while(count != self.Const_Cali_Reduce_Num + 1 ):
+            copy_img = img.copy()
+            count = count + 1
 
-        self.display_canvas(self.Const_Cali_Window_name, copy_img)
-        count = count + 1
+            self.draw_circle(copy_img, current_point, self.Const_Cali_Radius)
+            self.draw_cross(copy_img, current_point)
 
-        if (count == (duration * self.Const_Cali_Unit_Time)):
-            self.resize_figure(img, end_point, self.Const_Cali_Radius, self.Const_Cali_Capture_Duration, )
-            return
+            current_point = (current_point[0] + move_once_x, current_point[1] + move_once_y)
 
-        th = threading.Timer(1 / self.Const_Cali_Unit_Time, self.move_figure,
-                             [img, start_point, end_point, updated_current_point, self.Const_Cali_Move_Duration,
-                              count])
-        th.daemon = True
-        th.start()
-
-    def resize_figure(self, img, point, current_radius, duration, count=0):
-
-        copy_img = img.copy()
-
-        to_resize_radius = self.Const_Cali_Radius - self.Const_Cali_Resize_Radius
-        resize_once_radius = to_resize_radius / (duration * self.Const_Cali_Unit_Time)
-
-        updated_current_radius = current_radius - resize_once_radius
-        self.draw_circle(copy_img, point, updated_current_radius)
-        self.draw_cross(copy_img, point)
-
-        self.display_canvas(self.Const_Cali_Window_name, copy_img)
-        count = count + 1
+            self.display_canvas(copy_img)
+            time.sleep(self.Const_Cali_Reduce_Sleep)
 
 
-
-        while (self.is_face_detect == False):
-            continue
-
-        # 캘리브레이션 순간!
-        if (count == (duration * self.Const_Cali_Unit_Time)):
-
-            ##########################################
-            # to-do : 눈의 좌표 저장
-            # idea : 개선점? : 캘리브레이션 중간에 값 저장해서 보정하는건 어떤가?
-
-            self.left_eyeball_captured_data.append(self.left_eyeball_centre)
-            self.right_eyeball_captured_data.append(self.right_eyeball_centre)
-
-            self.left_iris_captured_data.append(self.left_iris_centre)
-            self.right_iris_captured_data.append(self.right_iris_centre)
-
-            print ("left_iris_captured_data : ",self.left_eyeball_captured_data)
-
-            ##########################################
-
-
-            # 다음 캘리브레이션 경로가 없다면 창 종료
-            if (self.sequence.empty() == True):
-                self.is_finish = True  # 종료플레그
-                print("Complete Calibration!!")
-                self.close_window(self.Const_Cali_Window_name)
-                return
-
-
-            # 큐에 다음 캘리브레이션 포인트가 있다면 원을 이동하여 캘리브레이션 작업
-            index = self.sequence.get_nowait()
-            self.move_figure(img, point, self.Cali_Center_Points[index], point, self.Const_Cali_Move_Duration)
-            return
-
-        th = threading.Timer(1 / self.Const_Cali_Unit_Time, self.resize_figure,
-                             [img, point, updated_current_radius, duration, count])
-        th.daemon = True
-        th.start()
 
     def init_cali(self,):
         # 캘리브레이션 포인인
@@ -209,7 +214,7 @@ class Calibration:
                 self.Cali_Center_Points.append(((int)(self.Const_Cali_Margin_X + cali_unit_distance_x * x)
                                            , (int)(self.Const_Cali_Margin_Y + cali_unit_distance_y * y)))
 
-    def init_canvas(self,):
+    def init_canvas(self):
         img = np.zeros((self.Const_Display_Y, self.Const_Display_X, 3), np.uint8)
         return img
 
@@ -225,15 +230,47 @@ class Calibration:
                        ((int)(point[0]), (int)(point[1] + half_size)),
                        (255, 255, 255), 1)
 
-    def display_canvas(self, canvas_name, img):
-        # Display On Canvas
-        if self.is_full_screen :
-            cv2.namedWindow(canvas_name, cv2.WINDOW_NORMAL)  # auto resized
-            cv2.setWindowProperty(canvas_name, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+    def display_canvas(self, img):
+        self.current_image=img
 
-        cv2.imshow(canvas_name, img)
-        None
 
-    def close_window(self, canvas_name):
-        # cv2.destroyWindow(canvas_name)
-        cv2.destroyAllWindows()
+
+#
+#
+#
+# if __name__ == '__main__':
+#
+#     # main
+#     cali = Calibration()
+#
+#     cali.is_face_detect = True
+#     th = threading.Thread(target=cali.start_cali)
+#     th.daemon = True
+#     th.start()
+#
+#     cv2.namedWindow("canvas")
+#
+#     while(True):
+#         if cali.current_image is not None:
+#
+#             cv2.imshow("canvas",cali.current_image)
+#             if cv2.waitKey(1) & 0xFF == ord('q'):
+#                 break
+#
+#
+#
+#
+#
+#
+#
+#
+
+
+
+
+
+
+
+
+
+
