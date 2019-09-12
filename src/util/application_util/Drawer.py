1057232e9597f43fa9bf560f97ec070f79e1ae61
chul <chuuuul@naver.com>
2019-09-13 02:25:50 +0900

    원형으로 변경, 색깔추가

--- src/util/application_util/Drawer.py
+++ src/util/application_util/Drawer.py
@@ -1,6 +1,6 @@
-from PyQt5.QtWidgets import QApplication, QWidget
-from PyQt5.QtGui import QPainter, QBrush, QPen, QPixmap
-from PyQt5.QtCore import Qt, QPointF,QObject,pyqtSignal,QRect
+
+from PyQt5.QtGui import QPainter, QBrush, QPen, QColor, QRadialGradient
+from PyQt5.QtCore import Qt, QPointF, QRect, QPoint
 
 
 class Drawer:
@@ -8,7 +8,6 @@ class Drawer:
     def draw_normal_rectangle(self,painter, x, y, width, height):
         painter.setBrush(QBrush(Qt.white, Qt.SolidPattern))
         painter.drawRect(QRect(x * width, y * height, width, height))
-
 
     def draw_growing_rectangle(self, painter, x, y, width, height, time_diff_rate):
         current_width = int(width * time_diff_rate)
@@ -18,6 +17,35 @@ class Drawer:
 
         painter.setBrush(QBrush(Qt.red, Qt.SolidPattern))
         painter.drawRect(QRect(current_position_x, current_position_y, current_width, current_height))
+
+    def draw_normal_circle(self, painter, x, y, width, height):
+        painter.setBrush(QBrush(QColor("#5c5c5c"), Qt.CrossPattern))
+        painter.drawEllipse(QPoint(x * width + int(width/2), y * height + int(height/2))
+                            ,int(height / 2) - 13, int(height/2) - 33)
+
+        painter.setBrush(QBrush(QColor("#ffffff"), Qt.SolidPattern))
+
+        painter.drawEllipse(QPoint(x * width + int(width/2), y * height + int(height/2))
+                            ,int(height / 2) - 20, int(height/2) - 40)
+
+
+
+    def draw_growing_circle(self, painter, x, y, width, height, time_diff_rate):
+        self.draw_normal_circle(painter, x, y , width, height)
+        # painter.setBrush(QBrush(QColor("#ffa8c5"), Qt.NoBrush))
+
+
+        radialGradient = QRadialGradient(QPoint(x * width + int(width/2),y * height + int(height/2))
+                                         ,int(height / 2) - 20,
+                                         QPoint(x * width + int(width/2),y * height + int(height/2))) # center,radius,focalPoint
+        radialGradient.setColorAt(0,QColor("#ffa6a6"))
+        radialGradient.setColorAt(0.7,QColor("#eb9494"))
+        radialGradient.setColorAt(1.0,QColor("#ff4d4d"))
+        painter.setBrush(QBrush(radialGradient))
+        painter.setPen(Qt.NoPen)
+        painter.drawEllipse(QPoint(x * width + int(width/2), y * height + int(height/2))
+                            ,(int(height / 2) - 20) * time_diff_rate, (int(height/2) - 40) * time_diff_rate)
+        painter.setPen(QPen())
 
 
     def draw_empty_circle(self, painter, x, y, width):
