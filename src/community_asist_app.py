032e017b611771a01621393b81e818ffb5234046
chul <chuuuul@naver.com>
2019-09-13 02:26:44 +0900

    깜빡이는 문제 해결 / 색깔추가 / 돌아가는 버튼 추가

--- src/community_asist_app.py
+++ src/community_asist_app.py
@@ -8,7 +8,7 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtWidgets import QApplication
 from PyQt5.QtGui import QPainter, QFont
 from PyQt5.QtCore import Qt, QRect
-from util.application_util.speak import speak_text
+from util.application_util.speak import Speak
 
 from random import *
 
@@ -24,8 +24,11 @@ class MyMain(QMainWindow):
     def __init__(self):
         super().__init__()
 
+        self.setStyleSheet("background-color: #d1d1d1")
+
         self.gazeDataReceiver = GazeDataReceiver(self)
         self.drawer = Drawer()
+        self.speak = Speak()
 
         gaze_receive_thread = threading.Thread(target = self.gazeDataReceiver.receive_gaze , name="gaze_receive_thread")
         gaze_receive_thread.daemon = True
@@ -35,7 +38,6 @@ class MyMain(QMainWindow):
         self.category_list = []
         self.item_list = []
         self.status = 0
-
 
         self.is_start = False
         self.is_speaking = False            # 말하는도중에는 그래픽 업데이트 못하도록
@@ -52,14 +54,8 @@ class MyMain(QMainWindow):
 
         self.font = QFont()
         self.font.setPixelSize(50)
-        self.parse_xml()
+        self.parse_xml()                    # 데이터가져오기
 
-
-        self.statusbar = self.statusBar()
-        self.mx = -10
-        self.my = -10
-
-        self.setMouseTracking(True)   # True 면, mouse button 안눌러도 , mouse move event 추적함
         self.setGeometry(300, 200, 1280, 720)
         self.show()
 
@@ -85,16 +81,6 @@ class MyMain(QMainWindow):
         self.window_height = QResizeEvent.size().height()
 
 
-
-
-
-    def mouseMoveEvent(self, event):
-        txt = "Mouse 위치 ; x={0},y={1}, global={2},{3}".format(event.x(), event.y(), event.globalX(), event.globalY())
-        self.mx = event.x()
-        self.my = event.y()
-        self.statusbar.showMessage(txt)
-        self.update()
-
     def create_random(self):
         while True:
             i = randint(1, 4)
@@ -111,34 +97,43 @@ class MyMain(QMainWindow):
 
 
     def paintEvent(self, event):
-        if self.is_start == False:
-            self.is_start = True
-            return
+
 
         if self.is_speaking == True:
             return
 
-        current_point = self.gazeDataReceiver.current_point
-
-        if self.previous_point is None:
-            self.previous_point = current_point
+        if self.is_start == False:
+            self.is_start = True
             return
 
         painter = QPainter(self)
 
-
+        current_point = self.gazeDataReceiver.current_point
         width = int(self.window_width / 3)
         height = int(self.window_height / 3)
 
         painter.setFont(self.font)
 
+        for index in range(1, 10):
+            x = int((index - 1) % 3)
+            y = int((index - 1) / 3)
 
+            # self.drawer.draw_normal_rectangle(painter, x, y, width, height)  # 전체_영역_색칠
+            self.drawer.draw_normal_circle(painter, x, y, width, height)
+
+            # 글자 표시
+            self.show_text(painter, x, y, width, height, index)
+
+
+        if self.previous_point is None:
+            self.previous_point = current_point
+            return
 
         # start time 측정
         if not self.is_checked:
             self.check_time()
 
-        # 보고있는_포인트가_바뀌면_종료시켜버림
+        # 보고있는_포인트가_바뀌면_return
         elif self.previous_point != current_point:
             self.previous_point = current_point
             self.uncheck_time()
@@ -167,15 +162,23 @@ class MyMain(QMainWindow):
 
             #아이템 선택
             elif self.status > 0:
+
+                if current_point == 9 :
+                    print("돌아가기")
+                    self.status = 0
+                    self.uncheck_time()
+                    self.update()
+                    return
+
                 if (current_point > len(self.item_list[self.status-1])):
-                    print("아이템 잘못선택!!!")
+                    print("비어있는 아이템")
                     self.uncheck_time()
                     self.update()
                     return
 
                 print("항목 선택 : ", self.item_list[self.status-1][current_point-1])
                 self.is_speaking = True
-                speak_text(self.item_list[self.status-1][current_point-1])
+                self.speak.speak_text(self.item_list[self.status-1][current_point-1])
                 self.is_speaking = False
                 self.status = 0
                 self.uncheck_time()
@@ -187,17 +190,19 @@ class MyMain(QMainWindow):
             time_diff_rate = round(time_diff / self.select_item_sec, 2)
             # print("rate : ", time_diff_rate)
 
-            for index in range(1, 10):
 
-                x = int((index - 1) % 3)
-                y = int((index - 1) / 3)
 
-                if index == current_point:
-                    self.drawer.draw_growing_rectangle(painter, x, y, width, height, time_diff_rate) # 보고있는_포인트_표시
-                else:
-                    self.drawer.draw_normal_rectangle(painter, x, y, width, height)  # 전체_영역_색칠
+            x = int((current_point - 1) % 3)
+            y = int((current_point - 1) / 3)
 
-                self.show_text(painter, x, y, width, height, index)
+            # self.drawer.draw_growing_rectangle(painter, x, y, width, height, time_diff_rate) # 보고있는_포인트_표시
+            self.drawer.draw_growing_circle(painter, x, y, width, height, time_diff_rate)
+
+
+
+
+            # 글자 표시
+            self.show_text(painter, x, y, width, height, current_point)
 
 
 
@@ -209,8 +214,12 @@ class MyMain(QMainWindow):
         # 서브레이어_선택_창
         elif self.status > 0:
             if (index - 1 < len(self.item_list[self.status-1])):      # item 항목 개수만큼만 표시
-                self.drawer.draw_text(painter, x, y, width, height, 
-                                  self.item_list[self.status - 1 ][index - 1]) 
+                self.drawer.draw_text(painter, x, y, width, height,
+                                  self.item_list[self.status - 1 ][index - 1])
+            elif index == 9 :
+                self.drawer.draw_text(painter, x, y, width, height, "돌아가기")
+
+
 
 
     def check_time(self):
@@ -238,20 +247,6 @@ class MyMain(QMainWindow):
                 list.append(sub.text)
             self.item_list.append(list)
 
-
-    #     #  Gaze값을 표현 할 때
-    #     if self.gazeDataReceiver.gaze_x is None or self.gazeDataReceiver.gaze_y is None :
-    #         return
-    #     painter = QPainter(self)
-    #     painter.setBrush(QBrush(Qt.green, Qt.SolidPattern))
-    #     painter.drawEllipse(QPointF( self.mx, self.my), 10, 10)
-    #
-    #
-    #
-    #     if self.once == False:
-    #         self.once = True
-    #         th = threading.Timer(1,self.send_emit)
-    #         th.start()
 
 
 if __name__ == "__main__":
